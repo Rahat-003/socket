9177fb6b2d1e58a11452aca94f09b8b565ad1e54
Akhtar Rahat <rahatakhter4@gmail.com>
2024-09-14 02:02:35 +0600

    eslint node. Show errors without red block line

--- server/app.js
+++ server/app.js
@@ -1,12 +1,21 @@
 import express from "express";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
-const app = express();
 const port = 3000;
+const app = express();
+
+const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! Server is running!");
+    res.send("Hello, World! Server is running! ");
+});
+
+io.on("connection", (socket) => {
+    console.log("User Connected");
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening on port ${port}`);
 });
