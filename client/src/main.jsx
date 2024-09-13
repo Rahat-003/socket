fec492d34e5c46004c88b96196c629e49edffe7a
Akhtar Rahat <rahatakhter4@gmail.com>
2024-09-14 02:13:39 +0600

    eslint

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import { CssBaseline } from "@mui/material";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <CssBaseline />
+        <App />
+    </React.StrictMode>
+);
--- server/app.js
+++ server/app.js
@@ -4,9 +4,11 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello, World! Server is running!");
+    x = 3;
+    hello = 10;
+    res.send("Hello, World! Server is running!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening on port ${port}`);
 });
