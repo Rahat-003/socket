e6dcb5bbb6b446de2d42ee27dce855b16c717227
Akhtar Rahat <rahatakhter4@gmail.com>
2024-09-14 02:03:50 +0600

    Revert "eslint node. Show errors without red block line"
    This reverts commit 9177fb6b2d1e58a11452aca94f09b8b565ad1e54.

--- server/app.js
+++ server/app.js
@@ -1,21 +1,12 @@
 import express from "express";
-import { Server } from "socket.io";
-import { createServer } from "http";
 
-const port = 3000;
 const app = express();
-
-const server = createServer(app);
-const io = new Server(server);
+const port = 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello, World! Server is running! ");
-});
-
-io.on("connection", (socket) => {
-    console.log("User Connected");
+  res.send("Hello, World! Server is running!");
 });
 
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
